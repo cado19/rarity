d1a652e49718785a921267b13ef64eee61c1562a
cado19 <mryoungrichie@gmail.com>
2024-10-14 13:14:46 +0300

    session script in driver module

--- drivers/all.php
+++ drivers/all.php
@@ -1,4 +1,8 @@
 <?php
+// THIS PAGE SHOWS ALL DRIVERS 
+
+// head to login screen if user is not signed in.
+include_once 'config/session_script.php';
 
 //page name. We set this inn the content start and also in the page title programatically
 $page = "Drivers";
