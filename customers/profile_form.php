b0056b0cd818bbd3899a0e87dbb3d900315b21a3
cado19 <mryoungrichie@gmail.com>
2024-10-21 14:42:12 +0300

    profile form and get details for booking voucher

--- customers/profile_form.php
+++ customers/profile_form.php
@@ -1,16 +1,29 @@
 <?php
-// THIS PAGE DISPLAYS ALLOWS A SELF REGISTERED CUSTOMER TO UPLOAD PROFILE IMAGE
-// head to login screen if user is not signed in.
-include_once 'config/session_script.php';
+    // THIS PAGE DISPLAYS DISPLAYS A FORM TO UPLOAD PROFILE IMAGE
+    // head to login screen if user is not signed in.
+    include_once 'config/session_script.php';
 
-$page = "Upload Profile Picture";
+    //page name. We set this inn the content start and also in the page title programatically
+    $page = "Upload Customer Profile";
 
-include_once 'partials/client-header.php';
-include_once 'partials/client-nav.php';
+    // Navbar Links. We set these link in the navbar programatically.
+    $home_link      = "index.php?page=customers/all";
+    $home_link_name = "All Customers";
 
-$vehicles = catalog_vehicles();
-$client = $_SESSION['client'];
-$id = $client['id'];
+    $new_link      = "index.php?page=customers/new";
+    $new_link_name = "New Customer";
+
+    // Breadcrumb variables for programatically setting breadcrumbs in content_start.php
+    $breadcrumb        = "Customers";
+    $breadcrumb_active = "Upload Profile";
+
+    // GET DRIVER ID FROM URL
+    if (isset($_GET['id'])) {
+        $id = $_GET['id'];
+    }
+
+    include_once 'partials/header.php';
+    include_once 'partials/content_start.php';
 ?>
 
 
@@ -21,7 +34,7 @@ $id = $client['id'];
 					<h3 class="card-title">Upload Profile Image</h3>
 				</div>
 				<div class="card-body">
-					<form action="index.php?page=client/profile/profile_upload" method="post" enctype="multipart/form-data">
+					<form action="index.php?page=customers/profile_upload" method="post" enctype="multipart/form-data">
 					    <div class="form-group">
 						    <label for="id_profile">Select Image Files to Upload:</label>
 						    <input type="hidden" name="id" value="<?php echo $id ?>">
